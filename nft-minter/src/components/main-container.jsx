e6aede9045b70474db2a778ad0b168b8700a8519
Nas <naserhamitou98@gmail.com>
2022-07-25 22:02:15 -0400

    .

--- nft-minter/src/components/main-container.jsx
+++ nft-minter/src/components/main-container.jsx
@@ -1,11 +1,33 @@
+import './main-container.css';
+
 const Main = () => {
     return(
-        <main>
-            <header></header>
-            <body>
-                <h1>Allo</h1>
-            </body>
-        </main>
+        <>
+            <main>
+                <header>
+                    <h1> NFT Minter !</h1>
+                </header>
+                <center>
+                    <div id="container">
+                        <div id="img-container">
+                            IMG IMG IMG IMGIMG IMG
+                            IMG IMGIMG IMGIMG IMG
+                            IMG IMGIMG IMGIMG IMGIMG 
+                            IMG IMGIMG IMGIMG IMGIMG IMG
+                            IMG IMGIMG IMGIMG IMGIMG IMG
+                            IMG IMGIMG IMGIMG IMGIMG IMG
+                            IMG IMGIMG IMGIMG IMGIMG IMG
+                            IMG IMGIMG IMGIMG IMGIMG IMG IMG
+                            IMG IMGIMG IMGIMG IMGIMG IMG
+                        </div>
+                        <div id="dashboard">
+                            <div id="loader"></div>
+                            <button>BUTTON</button>
+                        </div>
+                    </div>
+                </center>
+            </main>
+        </>
     )
 }
 
