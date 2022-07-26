739876de96a30f23e1730d0d3e7b42d70f041ec4
NaserHamitou <nas.hmt44@gmail.com>
2022-07-25 20:14:21 -0400

    Project init

---
+++ nft-minter/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Main from './components/main-container';
+
+function App() {
+  return (
+    <>
+      <Main></Main>
+    </>
+  );
+}
+
+export default App;
---
+++ nft-minter/src/components/main-container.jsx
@@ -0,0 +1,12 @@
+const Main = () => {
+    return(
+        <main>
+            <header></header>
+            <body>
+                <h1>Allo</h1>
+            </body>
+        </main>
+    )
+}
+
+export default Main;
